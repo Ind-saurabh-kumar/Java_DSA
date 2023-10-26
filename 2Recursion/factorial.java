5e1855ebdef4b7fcd92bb4eb222960bc50ee9949
saurabhkumar.conn@gmail.com <saurabhkumar.conn@gmail.com>
2023-10-26 13:44:26 +0530

    factorial

--- 2Recursion/factorial.java
+++ 2Recursion/factorial.java
@@ -2,7 +2,7 @@ public class factorial{
     public static void main(String args[]){
         // Instance of factorial class or Main class
         factorial recursion = new factorial();
-        var rec = recursion.fact(-5);
+        var rec = recursion.fact(5);
         System.out.println(rec);
     }
 
@@ -10,7 +10,7 @@ public class factorial{
     public int fact(int n){
         // uninitial cask check 
         if(n<1){
-            System.out.println("Please Enter Positive Number");
+           return 1;
         }
         // Base case check
         if(n==0 || n==1){

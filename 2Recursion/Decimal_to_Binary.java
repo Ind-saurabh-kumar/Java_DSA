466ac74a6c2a562fb2f77320d66e1fb753e23914
saurabhkumar.conn@gmail.com <saurabhkumar.conn@gmail.com>
2023-11-01 08:09:57 +0530

    Recursion_Updated

--- 2Recursion/Decimal_to_Binary.java
+++ 2Recursion/Decimal_to_Binary.java
@@ -1,12 +1,25 @@
 public class Decimal_to_Binary {
     public static void main(String []args){
 
+        Decimal_to_Binary recursion = new Decimal_to_Binary();
+        var result = recursion.dtb(23);
+        System.out.println(result);
+
         
 
     }
     
 
     public int dtb(int n){
-        return -1;
+
+        if(n<0)
+        {
+            return -1;  // error condition, not a valid decimal number
+        }
+        if(n==0)
+        {
+            return 0;
+        }
+        return n%2+ 10*dtb(n/2);
     }
 }
